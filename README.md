1833db821950a6c298c5e86e35fb2da6719bbf32
Pedro Henrique <phjfarias@hotmail.com>
2020-06-09 12:06:02 -0300

    Update readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# A API made with python and MongoDB
+# API em Python com Flask e MongoDb 
+
+API construida em python para prover todo o backend da aplicação do projeto em Neo3.
--- server.py
+++ server.py
@@ -1,14 +1,11 @@
 from flask import Flask, jsonify
 
-
 app = Flask(__name__)
 app.config["DEBUG"] = True # DEBUG MODE
-
 
 @app.route("/", methods=["GET"])
 def home():
     return "Vou classificar medidor"
-
 
 if __name__ == "__main__":
     app.run()
