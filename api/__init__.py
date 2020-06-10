af63ba34e9681ac10fd67aae35b206802f224ebe
Pedro Henrique <phjfarias@hotmail.com>
2020-06-10 00:33:59 -0300

    Connect with mongoDb

--- api/__init__.py
+++ api/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask, jsonify
+from api.config import MONGO_URI
+import pymongo
 
 # App configurations
 app = Flask(__name__)
 app.config["DEBUG"] = True  # DEBUG MODE
+app.config["MONGO_URI"] = MONGO_URI
+
+client = pymongo.MongoClient(MONGO_URI)
+db = client.get_database('Neo3')
---
+++ api/example_config.py
@@ -0,0 +1,10 @@
+"""
+This file is just an exmaple 
+To execute this application you need to create a config file with these variables
+
+api/
+    | config.py
+    | example_config.py 
+"""
+
+MONGO_URI = "Enter your mongo url here"
