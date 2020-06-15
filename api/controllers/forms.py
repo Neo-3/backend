6748e8f7e861e883a44c8aca956a1f3eb6b9f4a5
Pedro Henrique <phjfarias@hotmail.com>
2020-06-14 21:39:14 -0300

    simple post example

--- api/controllers/forms.py
+++ api/controllers/forms.py
@@ -1,7 +1,20 @@
-from api import app, jsonify
+from api import app, jsonify, request
 from api.models import Teste
 
 @app.route("/")
 def index():
     teste = Teste.objects()
     return teste.to_json()
+
+
+@app.route("/register/", methods=["POST"])
+def testPost():
+    body = request.get_json()
+    print(body.get("name"))
+
+    test = Teste(teste=body.get("name"))
+    try:
+        test.save()
+        return jsonify({'data': test.teste})
+    except Exception as error:
+        return jsonify({'Error': error})
